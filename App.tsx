1be7a9cb0248e2044a478bd904d66bba5ccbbf62
Alex Fomushkin <alex.fomushkin@qvik.fi>
2019-06-15 12:40:58 +0300

    Add eslint and prettier

--- App.tsx
+++ App.tsx
@@ -1,22 +1,20 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
- * 
+ *
  * @format
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+})
 
 interface Props {}
 export default class App extends Component<Props> {
@@ -27,7 +25,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -48,4 +46,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
---
+++ README.md
@@ -0,0 +1,4 @@
+TS setup: 
+https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
+Tests for TS:
+https://medium.com/@myezasifiso11/react-native-typescript-scaffolding-763ac2f9dd51
