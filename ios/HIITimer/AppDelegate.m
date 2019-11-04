c0d8064e925e130ba1a5e49f002e40ce2efb71a9
Aleksefo <aleksefo@gmail.com>
2019-11-04 22:16:16 +0200

    Add AppCenter

--- ios/HIITimer/AppDelegate.m
+++ ios/HIITimer/AppDelegate.m
@@ -12,11 +12,19 @@
 #import <React/RCTRootView.h>
 
 #import "RNBootSplash.h"
+#import <AppCenterReactNativeShared/AppCenterReactNativeShared.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"HIITimer"
